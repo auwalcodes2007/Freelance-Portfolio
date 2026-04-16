052de2063969a8330dec9eb37916359bbd0399d1
Auwal <mohammedauwalhassan07@gmail.com>
2026-04-16 19:28:53 +0100

    feat: upgrade scraper to loop through all pages, not just 1

--- project-1-scraper/scraper.py
+++ project-1-scraper/scraper.py
@@ -2,26 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 
-# Fetch the page and parse HTML
-url = "https://books.toscrape.com/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, "html.parser")
-
-# Find all books
-books = soup.find_all("article", class_="product_pod")
-
-# Create Excel Workbook
+# SETUP
+base_url = "https://books.toscrape.com/catalogue/page-{}.html"
 wb = openpyxl.Workbook()
 ws = wb.active
 ws.title = "Books"
 ws.append(["Title", "Price"])
 
+page = 1
 # Loop through and extract data
-for book in books:
-    name = book.h3.a.get("title")
-    price = book.select_one(".price_color").text
-    ws.append([name, price])
+while True:
+    url = base_url.format(page)
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        break
+
+    soup = BeautifulSoup(response.content, "html.parser")
+    # Find all books
+    books = soup.find_all("article", class_="product_pod")
+    for book in books:
+        name = book.h3.a.get("title")
+        price = book.select_one(".price_color").text
+        ws.append([name, price])
+
+    print(f"✅ Scraped page {page}")
+    page += 1
+
 
 # Save the excel file
 wb.save("books.xlsx")
-print("Done! books.xlsx has been saved.")
+print(f"Done! Scraped {page - 1} pages. books.xlsx saved.")
