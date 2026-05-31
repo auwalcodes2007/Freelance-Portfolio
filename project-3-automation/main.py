802853a3b5d39731b8cce9d74e21b64a79e2f260
Auwal <mohammedauwalhassan07@gmail.com>
2026-05-31 15:13:38 +0100

    feat: load credentials and write out tasks for project

---
+++ project-3-automation/main.py
@@ -0,0 +1,21 @@
+from dotenv import load_dotenv
+import os
+import openpyxl
+
+
+# LOAD CREDENTIALS
+load_dotenv()
+EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
+
+# --- READ CONTACTS FROM EXCEL ---
+
+
+
+# --- BUILD PERSONALIZED EMAIL ---
+
+
+# --- SEND EMAILS ---
+
+
+# --- MAIN ---
