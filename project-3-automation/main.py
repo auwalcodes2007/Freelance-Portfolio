8863d2f373db9c18eea03ff6bfd15e580027399b
Auwal <mohammedauwalhassan07@gmail.com>
2026-05-31 15:33:14 +0100

    feat: read contacts from excel and store in a list of dicts

--- project-3-automation/main.py
+++ project-3-automation/main.py
@@ -9,7 +9,15 @@ EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
 # --- READ CONTACTS FROM EXCEL ---
-
+def load_contacts(file_path):
+    wb = openpyxl.load_workbook(file_path)
+    ws = wb.active()
+    contacts = []
+    for row in ws.iter_rows(min=2, values_only=True):
+        name, email = row
+        if name and email:
+            contacts.append({"name": name, "email": email})
+    return contacts
 
 
 # --- BUILD PERSONALIZED EMAIL ---
